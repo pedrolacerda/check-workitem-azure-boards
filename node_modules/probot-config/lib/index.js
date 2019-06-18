e41dfbe509f296b5f624e1818a455da0267f4ce8
Pedro Lacerda <pedrolacerda@github.com>
2019-06-18 15:25:29 -0300

    Moves ADO config variables to ado.yml

---
+++ node_modules/probot-config/lib/index.js
@@ -0,0 +1,139 @@
+const path = require('path');
+const yaml = require('js-yaml');
+const merge = require('deepmerge');
+
+const CONFIG_PATH = '.github';
+const BASE_KEY = '_extends';
+const BASE_REGEX = new RegExp(
+  '^' +
+  '(?:([a-z\\d](?:[a-z\\d]|-(?=[a-z\\d])){0,38})/)?' + // org
+  '([-_.\\w\\d]+)' + // project
+  '(?::([-_./\\w\\d]+\\.ya?ml))?' + // filename
+    '$',
+  'i'
+);
+const DEFAULT_BASE = '.github';
+
+/**
+ * Decodes and parses a YAML config file
+ *
+ * @param {string} content Base64 encoded YAML contents
+ * @returns {object} The parsed YAML file as native object
+ */
+function parseConfig(content) {
+  return yaml.safeLoad(Buffer.from(content, 'base64').toString()) || {};
+}
+
+/**
+ * Merges an array of configs
+ *
+ * @param {array<config>} configs The configs to merge
+ * @param {object} options https://github.com/KyleAMathews/deepmerge#options
+ * @returns {object} The merged configuration
+ */
+function deepMergeConfigs(configs, options) {
+  return merge.all(configs.filter(config => config), options);
+}
+
+/**
+ * Loads a file from GitHub
+ *
+ * @param {Context} context A Probot context
+ * @param {object} params Params to fetch the file with
+ * @returns {Promise<object>} The parsed YAML file
+ * @async
+ */
+async function loadYaml(context, params) {
+  try {
+    const response = await context.github.repos.getContents(params);
+    return parseConfig(response.data.content);
+  } catch (e) {
+    if (e.code === 404) {
+      return null;
+    }
+
+    throw e;
+  }
+}
+
+/**
+ * Computes parameters for the repository specified in base
+ *
+ * Base can either be the name of a repository in the same organization or
+ * a full slug "organization/repo".
+ *
+ * @param {object} params An object containing owner, repo and path
+ * @param {string} base A string specifying the base repository
+ * @returns {object} The params of the base configuration
+ */
+function getBaseParams(params, base) {
+  if (typeof base !== 'string') {
+    throw new Error(`Invalid repository name in key "${BASE_KEY}"`);
+  }
+
+  const match = base.match(BASE_REGEX);
+  if (match == null) {
+    throw new Error(`Invalid repository name in key "${BASE_KEY}": ${base}`);
+  }
+
+  return {
+    owner: match[1] || params.owner,
+    repo: match[2],
+    path: match[3] || params.path,
+  };
+}
+
+/**
+ * Loads the specified config file from the context's repository
+ *
+ * If the config file contains a top-level key "_extends", it is merged
+ * with a config of the same name in the repository specified by the
+ * _extends key.  The repository of the base configuration can either be
+ * given as "repository" or as "organization/repository".
+ *
+ * If the config file does not exist in the context's repository, `null`
+ * is returned. If the base repository does not exist or does not contain
+ * the config file, it is ignored.
+ *
+ * If a default config is given, it is merged with the config from the
+ * repository, if it exists.
+ *
+ * @param {Context} context A Probot context
+ * @param {string} fileName Name of the config file
+ * @param {object} defaultConfig A default config that is merged in
+ * @param {object} deepMergeOptions from deepmerge
+ * @returns {object} The merged configuration
+ * @async
+ */
+async function getConfig(context, fileName, defaultConfig, deepMergeOptions) {
+  const filePath = path.posix.join(CONFIG_PATH, fileName);
+  const params = context.repo({
+    path: filePath,
+  });
+
+  const config = await loadYaml(context, params);
+  let baseRepo;
+  if (config == null) {
+    baseRepo = DEFAULT_BASE;
+  } else if (config != null && BASE_KEY in config) {
+    baseRepo = config[BASE_KEY];
+    delete config[BASE_KEY];
+  }
+
+  let baseConfig;
+  if (baseRepo) {
+    const baseParams = getBaseParams(params, baseRepo);
+    baseConfig = await loadYaml(context, baseParams);
+  }
+
+  if (config == null && baseConfig == null && !defaultConfig) {
+    return null;
+  }
+
+  return deepMergeConfigs(
+    [defaultConfig, baseConfig, config],
+    deepMergeOptions
+  );
+}
+
+module.exports = getConfig;
---
+++ node_modules/probot-config/lib/index.test.js
@@ -0,0 +1,371 @@
+/* eslint-env jest */
+const getConfig = require('./index');
+
+const { fn } = jest;
+
+function mockContext(getContents) {
+  return {
+    repo(params) {
+      return Object.assign(
+        {
+          owner: 'owner',
+          repo: 'repo',
+        },
+        params
+      );
+    },
+
+    github: {
+      repos: {
+        async getContents(params) {
+          const content = Buffer.from(getContents(params)).toString('base64');
+          return {
+            data: {
+              content,
+            },
+          };
+        },
+      },
+    },
+  };
+}
+
+function mockError(code) {
+  const err = new Error('Not found');
+  err.code = code;
+  throw err;
+}
+
+test('returns null on 404', async () => {
+  const spy = fn()
+    .mockImplementationOnce(() => mockError(404))
+    .mockImplementationOnce(() => mockError(404))
+    .mockImplementationOnce(() => mockError(500));
+
+  const config = await getConfig(mockContext(spy), 'test.yml');
+  expect(config).toEqual(null);
+
+  expect(spy).toHaveBeenCalledTimes(2);
+  expect(spy).toHaveBeenCalledWith({
+    owner: 'owner',
+    repo: 'repo',
+    path: '.github/test.yml',
+  });
+  expect(spy).toHaveBeenLastCalledWith({
+    owner: 'owner',
+    repo: '.github',
+    path: '.github/test.yml',
+  });
+});
+
+test('loads a direct config', async () => {
+  const spy = fn()
+    .mockImplementationOnce(() => 'foo: foo')
+    .mockImplementationOnce(() => mockError(500));
+
+  const config = await getConfig(mockContext(spy), 'test.yml');
+  expect(config).toEqual({
+    foo: 'foo',
+  });
+
+  expect(spy).toHaveBeenCalledTimes(1);
+  expect(spy).toHaveBeenLastCalledWith({
+    owner: 'owner',
+    repo: 'repo',
+    path: '.github/test.yml',
+  });
+});
+
+test('merges the default config', async () => {
+  const spy = fn()
+    .mockImplementationOnce(() => 'foo: foo')
+    .mockImplementationOnce(() => mockError(500));
+
+  const config = await getConfig(mockContext(spy), 'test.yml', {
+    def: true,
+  });
+  expect(config).toEqual({
+    foo: 'foo',
+    def: true,
+  });
+
+  expect(spy).toHaveBeenCalledTimes(1);
+  expect(spy).toHaveBeenLastCalledWith({
+    owner: 'owner',
+    repo: 'repo',
+    path: '.github/test.yml',
+  });
+});
+
+test('merges a base config', async () => {
+  const spy = fn()
+    .mockImplementationOnce(() => 'foo: foo\nbar: bar\n_extends: base')
+    .mockImplementationOnce(() => 'bar: bar2\nbaz: baz');
+
+  const config = await getConfig(mockContext(spy), 'test.yml');
+  expect(config).toEqual({
+    foo: 'foo',
+    bar: 'bar',
+    baz: 'baz',
+  });
+
+  expect(spy).toHaveBeenCalledTimes(2);
+  expect(spy).toHaveBeenCalledWith({
+    owner: 'owner',
+    repo: 'repo',
+    path: '.github/test.yml',
+  });
+  expect(spy).toHaveBeenLastCalledWith({
+    owner: 'owner',
+    repo: 'base',
+    path: '.github/test.yml',
+  });
+});
+
+test('merges the base and default config', async () => {
+  const spy = fn()
+    .mockImplementationOnce(() => 'foo: foo\n_extends: base')
+    .mockImplementationOnce(() => 'bar: bar');
+
+  const config = await getConfig(mockContext(spy), 'test.yml', {
+    def: true,
+  });
+  expect(config).toEqual({
+    foo: 'foo',
+    bar: 'bar',
+    def: true,
+  });
+
+  expect(spy).toHaveBeenCalledTimes(2);
+  expect(spy).toHaveBeenCalledWith({
+    owner: 'owner',
+    repo: 'repo',
+    path: '.github/test.yml',
+  });
+  expect(spy).toHaveBeenLastCalledWith({
+    owner: 'owner',
+    repo: 'base',
+    path: '.github/test.yml',
+  });
+});
+
+test('merges a base config from another organization', async () => {
+  const spy = fn()
+    .mockImplementationOnce(() => 'foo: foo\nbar: bar\n_extends: other/base')
+    .mockImplementationOnce(() => 'bar: bar2\nbaz: baz');
+
+  const config = await getConfig(mockContext(spy), 'test.yml');
+  expect(config).toEqual({
+    foo: 'foo',
+    bar: 'bar',
+    baz: 'baz',
+  });
+
+  expect(spy).toHaveBeenCalledTimes(2);
+  expect(spy).toHaveBeenCalledWith({
+    owner: 'owner',
+    repo: 'repo',
+    path: '.github/test.yml',
+  });
+  expect(spy).toHaveBeenLastCalledWith({
+    owner: 'other',
+    repo: 'base',
+    path: '.github/test.yml',
+  });
+});
+
+test('merges a base config with a custom path', async () => {
+  const spy = fn()
+    .mockImplementationOnce(() => 'foo: foo\nbar: bar\n_extends: base:test.yml')
+    .mockImplementationOnce(() => 'bar: bar2\nbaz: baz');
+
+  const config = await getConfig(mockContext(spy), 'test.yml');
+  expect(config).toEqual({
+    foo: 'foo',
+    bar: 'bar',
+    baz: 'baz',
+  });
+
+  expect(spy).toHaveBeenCalledTimes(2);
+  expect(spy).toHaveBeenCalledWith({
+    owner: 'owner',
+    repo: 'repo',
+    path: '.github/test.yml',
+  });
+  expect(spy).toHaveBeenLastCalledWith({
+    owner: 'owner',
+    repo: 'base',
+    path: 'test.yml',
+  });
+});
+
+test('ignores a missing base config', async () => {
+  const spy = fn()
+    .mockImplementationOnce(() => 'foo: foo\nbar: bar\n_extends: base')
+    .mockImplementationOnce(() => mockError(404));
+
+  const config = await getConfig(mockContext(spy), 'test.yml');
+  expect(config).toEqual({
+    foo: 'foo',
+    bar: 'bar',
+  });
+
+  expect(spy).toHaveBeenCalledTimes(2);
+  expect(spy).toHaveBeenCalledWith({
+    owner: 'owner',
+    repo: 'repo',
+    path: '.github/test.yml',
+  });
+  expect(spy).toHaveBeenLastCalledWith({
+    owner: 'owner',
+    repo: 'base',
+    path: '.github/test.yml',
+  });
+});
+
+test('loads an empty config', async () => {
+  const spy = fn()
+    .mockImplementationOnce(() => '')
+    .mockImplementationOnce(() => mockError(500));
+
+  const config = await getConfig(mockContext(spy), 'test.yml');
+  expect(config).toEqual({});
+
+  expect(spy).toHaveBeenCalledTimes(1);
+  expect(spy).toHaveBeenCalledWith({
+    owner: 'owner',
+    repo: 'repo',
+    path: '.github/test.yml',
+  });
+});
+
+test('throws on error', async () => {
+  try {
+    expect.assertions(1);
+    const spy = jest.fn().mockImplementation(() => mockError(500));
+    await getConfig(mockContext(spy), 'test.yml');
+  } catch (e) {
+    expect(e.code).toBe(500);
+  }
+});
+
+test('throws on a non-string base', async () => {
+  const spy = fn()
+    .mockImplementationOnce(() => 'foo: foo\nbar: bar\n_extends: { nope }')
+    .mockImplementationOnce(() => mockError(500));
+
+  try {
+    expect.assertions(1);
+    await getConfig(mockContext(spy), 'test.yml');
+  } catch (e) {
+    expect(e.message).toMatch(/invalid/i);
+  }
+});
+
+test('throws on an invalid base', async () => {
+  const spy = fn()
+    .mockImplementationOnce(() => 'foo: foo\nbar: bar\n_extends: "nope:"')
+    .mockImplementationOnce(() => mockError(500));
+
+  try {
+    expect.assertions(1);
+    await getConfig(mockContext(spy), 'test.yml');
+  } catch (e) {
+    expect(e.message).toMatch(/nope:/);
+  }
+});
+
+test('uses the .github directory on a .github repo', async () => {
+  const spy = fn()
+    .mockImplementationOnce(() => 'foo: foo\nbar: bar\n_extends: .github')
+    .mockImplementationOnce(() => 'bar: bar2\nbaz: baz');
+
+  const config = await getConfig(mockContext(spy), 'test.yml');
+  expect(config).toEqual({
+    foo: 'foo',
+    bar: 'bar',
+    baz: 'baz',
+  });
+
+  expect(spy).toHaveBeenCalledTimes(2);
+  expect(spy).toHaveBeenCalledWith({
+    owner: 'owner',
+    repo: 'repo',
+    path: '.github/test.yml',
+  });
+  expect(spy).toHaveBeenLastCalledWith({
+    owner: 'owner',
+    repo: '.github',
+    path: '.github/test.yml',
+  });
+});
+
+test('defaults to .github repo if no config found', async () => {
+  const spy = fn()
+    .mockImplementationOnce(() => mockError(404))
+    .mockImplementationOnce(() => 'foo: foo\nbar: bar');
+
+  const config = await getConfig(mockContext(spy), 'test.yml');
+  expect(config).toEqual({
+    foo: 'foo',
+    bar: 'bar',
+  });
+
+  expect(spy).toHaveBeenCalledTimes(2);
+  expect(spy).toHaveBeenCalledWith({
+    owner: 'owner',
+    repo: 'repo',
+    path: '.github/test.yml',
+  });
+  expect(spy).toHaveBeenLastCalledWith({
+    owner: 'owner',
+    repo: '.github',
+    path: '.github/test.yml',
+  });
+});
+
+test('deep merges the base config', async () => {
+  const spy = fn()
+    .mockImplementationOnce(
+      () => 'obj:\n  foo:\n  - name: master\n_extends: .github'
+    )
+    .mockImplementationOnce(() => 'obj:\n  foo:\n  - name: develop');
+
+  const config = await getConfig(mockContext(spy), 'test.yml');
+
+  expect(config).toEqual({
+    obj: {
+      foo: [
+        {
+          name: 'develop',
+        },
+        {
+          name: 'master',
+        },
+      ],
+    },
+  });
+});
+
+test('accepts deepmerge options', async () => {
+  const spy = fn()
+    .mockImplementationOnce(
+      () =>
+        'foo:\n  - name: master\n    shouldChange: changed\n_extends: .github'
+    )
+    .mockImplementationOnce(
+      () =>
+        'foo:\n  - name: develop\n  - name: master\n    shouldChange: should'
+    );
+
+  const customMerge = fn().mockImplementationOnce();
+  await getConfig(
+    mockContext(spy),
+    'test.yml',
+    {},
+    {
+      arrayMerge: customMerge,
+    }
+  );
+  expect(customMerge).toHaveBeenCalled();
+});
